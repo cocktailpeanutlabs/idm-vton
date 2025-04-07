972bdddc15834873b4c8944044cd2d4713de5340
6Morpheus6 <wanton-decay@gmx.net>
2025-04-07 23:50:45 +0200

    move dependencies to requirements.txt

--- install.js
+++ install.js
@@ -28,8 +28,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install huggingface_hub==0.25.2 gradio devicetorch",
-          "pip install -r requirements.txt"
+          "uv pip install -r requirements.txt"
         ]
       }
     },
